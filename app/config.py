dba77d6934ca141a15c66a6f1b430e0e73c0b383
Ahmedtaha <Ahmedbinaltaha@gmail.com>
2026-08-29 00:23:33 +0300

    creating settings

---
+++ app/config.py
@@ -0,0 +1,17 @@
+import os 
+from dotenv import load_dotenv
+
+
+class Settings:
+    GROQ_API_KEY= os.getenv("GROQ_API_KEY")
+    GROQ_MODEL = "llama-3.3-70b-versatile"
+    
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+
+    QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
+    QDRANT_URL = os.getenv("QDRANT_CLUSTER_ENDPOINT")
+    QDRANT_COLLECTION = "enterprise_rag"
+    
+    
+settings=Settings()
